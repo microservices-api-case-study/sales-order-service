5a807fc1d19b39eafea18c75c85811e431be34c8
Anand <50521705+arockiaanandraj@users.noreply.github.com>
2019-11-24 20:15:40 +0530

    Initial Commit

---
+++ src/main/java/com/retail/services/salesorderservice/SalesOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.retail.services.salesorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesOrderServiceApplication.class, args);
+	}
+
+}

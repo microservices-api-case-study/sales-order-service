f61278f570c8a90fb21ea39c6274d6fd5c7f6a6c
Anand <50521705+arockiaanandraj@users.noreply.github.com>
2019-11-25 00:51:01 +0530

    Updated Controller

--- src/main/java/com/retail/services/salesorderservice/ItemService.java
+++ src/main/java/com/retail/services/salesorderservice/ItemService.java
@@ -2,9 +2,15 @@ package com.retail.services.salesorderservice;
 
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name = "item-service")
 @RibbonClient(name = "item-service")
+@RequestMapping("/service2")
 public interface ItemService {
 
+	@GetMapping("/items/{itemname}")
+	public OrderLineItem getItemByName(@PathVariable("itemname") String itemName);
 }
--- src/main/java/com/retail/services/salesorderservice/SalesOrderController.java
+++ src/main/java/com/retail/services/salesorderservice/SalesOrderController.java
@@ -1,5 +1,22 @@
 package com.retail.services.salesorderservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service3")
 public class SalesOrderController {
+
+	@Autowired
+	ItemService itemService;
+
+	@GetMapping("/orders")
+	public String createOrder() {
+		System.out.println(itemService.getItemByName("Pen"));
+		System.out.println(itemService.getItemByName("Paper"));
+		return "success";
+	}
 
 }

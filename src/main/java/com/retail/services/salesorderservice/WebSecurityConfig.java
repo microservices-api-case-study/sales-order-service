9049384fd447b9582a21fbaa22316fd8f5c0d3c0
arockiaanandraj <arockiaanandraj@gmail.com>
2019-11-30 07:38:47 +0530

    Added PCF flavored config client and service registry

---
+++ src/main/java/com/retail/services/salesorderservice/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.retail.services.salesorderservice;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@ConditionalOnProperty(value = "app.security.basic.enabled", havingValue = "false")
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll().anyRequest().authenticated();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().anyRequest();
+	}
+}
